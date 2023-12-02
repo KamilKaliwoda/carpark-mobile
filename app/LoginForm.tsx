eb00b58987877542ebf6b8ad43336bba908c0290
kkaliwoda <kamil.kaliwoda@antsolutions.eu>
2023-12-02 13:38:19 +0100

    login page

---
+++ app/LoginForm.tsx
@@ -0,0 +1,21 @@
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { styles } from './styles';
+
+
+export const LoginForm = (props: any): JSX.Element => {
+    return (
+        <View style={styles.LoginView}>
+            <Text style={styles.LoginTitleText}>Log in account</Text>
+            <View style={styles.LoginForm}>
+                <Text style={styles.LoginNormalText}>username</Text>
+                <TextInput style={styles.LoginTextInput} placeholder='username'/>
+                <Text style={styles.LoginNormalText}>password</Text>
+                <TextInput style={styles.LoginTextInput} placeholder='password'/>
+                <TouchableOpacity style={styles.LoginButtonBackground}>
+                    <Text style={styles.LoginButtonText}>Log in</Text>
+                </TouchableOpacity>
+            </View>
+      </View>
+    );
+  };
+  
---
+++ app/styles.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+    LoginView: {
+        flex: 1,
+        backgroundColor: 'purple',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    LoginForm: {
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        width: '60%',
+        height: '50%',
+    },
+    LoginTextInput: {
+        backgroundColor: 'white',
+    },
+    LoginTitleText: {
+        color: 'white',
+    },
+    LoginNormalText: {
+        color: 'white',
+    },
+    LoginButtonBackground: {
+        backgroundColor: 'white',
+    },
+    LoginButtonText: {
+        color: 'purple',
+    },
+  });
+  
