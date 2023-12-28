8120958d21681a35fcd4a5468a552bf5591db67b
Kamil Kaliwoda <kaliwoda@student.agh.edu.pl>
2023-12-28 21:23:55 +0100

    hide keyboard when field outside input clicked

--- src/screens/LoginForm.tsx
+++ src/screens/LoginForm.tsx
@@ -1,4 +1,4 @@
-import { Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { styles } from '../styles/LoginForm';
 import { validateLogin } from '../services/LoginForm';
 import { useState } from 'react';
@@ -7,18 +7,20 @@ import { useState } from 'react';
 export const LoginForm = (props: any): JSX.Element => {
     const [password, setPassword] = useState('');
     return (
-        <View style={styles.LoginView}>
-            <Text style={styles.LoginTitleText}>Log in account</Text>
-            <View style={styles.LoginForm}>
-                <Text style={styles.LoginNormalText}>username</Text>
-                <TextInput style={styles.LoginTextInput} placeholder='username' autoComplete={'username'} onChangeText={newUsername => props.onChangeUsername(newUsername)}/>
-                <Text style={styles.LoginNormalText}>password</Text>
-                <TextInput style={styles.LoginTextInput} placeholder='password' secureTextEntry={true} autoComplete={'current-password'} onChangeText={newPassword => setPassword(newPassword)}/>
-                <TouchableOpacity style={styles.LoginButtonBackground} onPress={() => validateLogin(props, props.username, password)}>
-                    <Text style={styles.LoginButtonText}>Log in</Text>
-                </TouchableOpacity>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+            <View style={styles.LoginView}>
+                <Text style={styles.LoginTitleText}>Log in account</Text>
+                <View style={styles.LoginForm}>
+                    <Text style={styles.LoginNormalText}>username</Text>
+                    <TextInput style={styles.LoginTextInput} placeholder='username' autoComplete={'username'} onChangeText={newUsername => props.onChangeUsername(newUsername)}/>
+                    <Text style={styles.LoginNormalText}>password</Text>
+                    <TextInput style={styles.LoginTextInput} placeholder='password' secureTextEntry={true} autoComplete={'current-password'} onChangeText={newPassword => setPassword(newPassword)}/>
+                    <TouchableOpacity style={styles.LoginButtonBackground} onPress={() => validateLogin(props, props.username, password)}>
+                        <Text style={styles.LoginButtonText}>Log in</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
-      </View>
+        </TouchableWithoutFeedback>
     );
   };
   
